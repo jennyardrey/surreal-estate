24e3fab2d871c011944d30b633e15dc54c89b22a
Jenny Ardrey <jenny@ardrey.co.uk>
2019-12-07 11:25:25 +0000

    added routes and links to navigation

---
+++ src/AddProperty.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class AddProperty extends Component {
+	render() {
+		return <div>Add Property Page</div>
+	}
+};
+
+export default AddProperty
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import '../components/Navbar.css';
 import "../components/images/logo.jpg";
+import { Link } from 'react-router-dom'
 
 class Navbar extends Component {
 	render() {
 		return < div className="Navbar" >
-			<img src="https://mcrcodes.s3.eu-west-2.amazonaws.com/course/surreal-estate/logo.png" alt="logo"></img>
+			<img className="logo" src="https://mcrcodes.s3.eu-west-2.amazonaws.com/course/surreal-estate/logo.png" alt="logo"></img>
 			<ul className="nav">
-				<li className="item">View Properties</li>
-				<li className="item">Add a Property</li>
+				<Link className="item" to="/"> View Properties </Link>
+				<Link className="item" to="/add-property">Add a Property </Link>
 			</ul>
 		</div >
 
